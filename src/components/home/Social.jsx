5a40c30c2042290cb6c51ff07ec54c970ccddda9
sajinmahesh <sajinm461@gmail.com>
2023-05-14 16:56:15 +0530

    initial commit

---
+++ src/components/home/Social.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Social = () => {
+  return (
+    <div className="home__social">
+      <a href="https://instagram.com/sajin_mahesh" className="home__social-icon" target="_blank ">
+        <i class="uil uil-instagram"></i>
+      </a>
+      <a href="https://www.linkedin.com/in/sajin-mahesh-79230b212" className="home__social-icon" target="_blank ">
+        <i class="uil uil-linkedin-alt"></i>
+      </a>
+      <a href="https://github.com/sajinmahesh" className="home__social-icon" target="_blank ">
+        <i class="uil uil-github-alt"></i>
+      </a>
+      <a href="https://www.facebook.com/sajin.kdz" className="home__social-icon" target="_blank ">
+        <i class="uil uil-facebook-messenger-alt"></i>
+      </a>
+    </div>
+  );
+};
+
+export default Social;
