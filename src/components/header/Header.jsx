c2ce07d97842a40dcdd2fe17d7e19498a4ef89ee
sajinmahesh <sajinm461@gmail.com>
2023-05-24 11:04:29 +0530

    about section changes

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,20 +1,55 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./header.css";
 
 const Header = () => {
   //----------change background header-------//
   window.addEventListener("scroll", function () {
     const header = document.querySelector(".header");
-    if (this.scrollY >= 80) header.classList.add("scroll__header");
+    if (this.scrollY > 80) header.classList.add("scroll__header");
     else header.classList.remove("scroll__header");
   });
+
+  useEffect(() => {
+    // Add scroll event listener to update active nav link
+    const handleScroll = () => {
+      const scrollPosition = window.scrollY;
+
+      const homeSection = document.getElementById("home");
+      const aboutSection = document.getElementById("about");
+      const skillsSection = document.getElementById("skills");
+      const educationSection = document.getElementById("education");
+      //const PortfolioSection = document.getElementById("portfolio");
+      const contactSection = document.getElementById("contact");
+
+      if (scrollPosition < aboutSection.offsetTop) {
+        setActiveNav("#home");
+      } else if (scrollPosition >= aboutSection.offsetTop && scrollPosition < skillsSection.offsetTop) {
+        setActiveNav("#about");
+      } else if (scrollPosition >= skillsSection.offsetTop && scrollPosition < educationSection.offsetTop) {
+        setActiveNav("#skills");
+      } else if (scrollPosition >= educationSection.offsetTop && scrollPosition < contactSection.offsetTop) {
+        setActiveNav("#education");
+        // } else if (scrollPosition >= PortfolioSection.offsetTop && scrollPosition < contactSection.offsetTop) {
+        //   setActiveNav("portfolio");
+      } else if (scrollPosition >= contactSection.offsetTop) setActiveNav("#contact");
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    // Clean up the event listener on component unmount
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+  const [activeNav, setActiveNav] = useState("#home");
   // ----toggle menu ------//
   const [Toggle, showMenu] = useState(false);
-  const [activeNav, setActvieNav] = useState("#home");
+
+  //const [activeNav, setActvieNav] = useState("#home");
   return (
     <header className="header">
       <nav className="nav container">
-        <a href="index.html" className="nav__logo">
+        <a href="index.html" className="nav__logo myname">
           &lt;Sajin &#47;&gt;
         </a>
         <div className={Toggle ? "nav__menu show-menu" : "nav__menu"}>
@@ -22,7 +57,7 @@ const Header = () => {
             <li className="nav__item">
               <a
                 href="#home"
-                onClick={() => setActvieNav("#home")}
+                onClick={() => setActiveNav("#home")}
                 className={activeNav === "#home" ? "nav__link active__link" : "nav__link"}
               >
                 <i className="uil uil-estate nav__icon"></i> Home
@@ -31,7 +66,7 @@ const Header = () => {
             <li className="nav__item">
               <a
                 href="#about"
-                onClick={() => setActvieNav("#about")}
+                onClick={() => setActiveNav("#about")}
                 className={activeNav === "#about" ? "nav__link active__link" : "nav__link"}
               >
                 <i className="uil uil-user nav__icon"></i> About
@@ -40,7 +75,7 @@ const Header = () => {
             <li className="nav__item">
               <a
                 href="#skills"
-                onClick={() => setActvieNav("#skills")}
+                onClick={() => setActiveNav("#skills")}
                 className={activeNav === "#skills" ? "nav__link active__link" : "nav__link"}
               >
                 <i className="uil uil-file-alt nav__icon"></i> Skills
@@ -49,25 +84,17 @@ const Header = () => {
             <li className="nav__item">
               <a
                 href="#education"
-                onClick={() => setActvieNav("#education")}
+                onClick={() => setActiveNav("#education")}
                 className={activeNav === "#education" ? "nav__link active__link" : "nav__link"}
               >
                 <i className="uil uil-briefcase-alt nav__icon"></i> Education
               </a>
             </li>
-            <li className="nav__item">
-              <a
-                href="#portflolio"
-                onClick={() => setActvieNav("#portfolio")}
-                className={activeNav === "#portfolio" ? "nav__link active__link" : "nav__link"}
-              >
-                <i className="uil uil-scenery nav__icon"></i> Portfolio
-              </a>
-            </li>
+
             <li className="nav__item">
               <a
                 href="#contact"
-                onClick={() => setActvieNav("#contact")}
+                onClick={() => setActiveNav("#contact")}
                 className={activeNav === "#contact" ? "nav__link active__link" : "nav__link"}
               >
                 <i className="uil uil-message nav__icon"></i> Contact
